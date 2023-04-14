f0db2e6ffbaf26e77e265354f9a078b3847ae01a
Nyandika Donald <donald.nyandika@gmail.com>
2023-04-14 09:52:54 +0000

    Increase test timeout
    Signed-off-by: Nyandika Donald <donald.nyandika@gmail.com>

--- e2e/sample.spec.ts
+++ e2e/sample.spec.ts
@@ -1,5 +1,9 @@
 import { expect, test } from '@playwright/test';
 
+test.beforeEach(async({page})=>{
+  await page.goto('/')
+})
+
 test('Playwright test', async ({ page }) => {
   await page.goto('https://www.example.com');
   const title = await page.title();
